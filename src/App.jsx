3842ec073234e20b4c20e8d67aa2462bea10b52d
avh17 <anirudhherady17@gmail.com>
2025-11-25 20:57:46 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div><h1 className='text-3xl font-bold underline'>Welcome to Anirudh's Portfolio</h1></div>
+  )
+}
+
+export default App
